6af099a5fbc57ab5a4a4f9f39374c50f678ef794
jsontype <jsontyper@gmail.com>
2023-01-29 19:29:57 +0900

    init

---
+++ src/components/Movies/index.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react"
+import "./style.css"
+
+export default function Movies() {
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    // api 호출
+    // fetch("https://yts.mx/api/v2/list_movies.json?sort_by=rating")
+    fetch("https://yts.mx/api/v2/list_movies.json")
+      .then((res) => res.json())
+      .then((json) => setMovies(json.data.movies))
+  }, [])
+
+  const render = movies.map((item) => {
+    return (
+      <div key={item.id}>
+        <a className="movieTitle" href={item.url}>
+          {item.title}
+        </a>
+      </div>
+    )
+  })
+
+  return <div>{render}</div>
+}
