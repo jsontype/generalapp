11e472626fcf3849c30485bec59f5ea9d4aa5967
Junyeob <jy@Junyeobui-MacBookAir.local>
2023-03-11 10:05:06 +0900

    refactor: counter앱에 redux 적용

--- src/components/Counter/index.tsx
+++ src/components/Counter/index.tsx
@@ -2,25 +2,23 @@ import styles from "./style.module.scss"
 
 type Counterprops = {
   count: number
-  setCount: (count: number) => void
+  onIncrease : (count: number) => void
+  onDecrease : (count: number) => void
 }
 
-export default function Counter({ count, setCount }: Counterprops) {
-  const onIncrease = () => {
-    setCount(count + 1)
-  }
-
-  const onDecrease = () => {
-    setCount(count - 1)
-  }
-
+export default function Counter({ 
+  count,
+  onIncrease,
+  onDecrease,
+ }: Counterprops) {
   return (
     <>
       <div className={styles.countLabel}>{count}</div>
-      <button className={styles.countBtn} onClick={onIncrease}>
+      <button className={styles.countBtn} onClick={(count: any) => onIncrease(count)}>
         +
       </button>
-      <button className={styles.countBtn} onClick={onDecrease}>
+      <button
+        className={styles.countBtn} onClick={(count: any) => onDecrease(count)}>
         -
       </button>
     </>
