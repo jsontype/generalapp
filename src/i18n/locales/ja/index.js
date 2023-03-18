83cd8544598642a1f59a0bada6b9676e2c2799d2
Junyeob <jy@Junyeobui-MacBookAir.local>
2023-03-18 23:20:33 +0900

    feat: Counter 다국어처리

--- src/i18n/locales/ja/index.js
+++ src/i18n/locales/ja/index.js
@@ -1,9 +1,11 @@
 import todos from "./todos.json"
+import counter from "./counter.json"
 
 // 주의 : function형으로 사용하면 안됨!
 const ja = {
   // 이후 json 파일을 추가해서 사용한다.
   todos,
+  counter,
 }
 
 export default ja
