01ccbdc9afe57a8104b43ae99e62cb5a12de8fa1
신기호 <singiho@kimuhyonsunoMacBook-Pro.local>
2023-03-24 02:12:48 +0900

    feat: Movies앱  다국어처리기능 추가

--- src/i18n/locales/ko/index.js
+++ src/i18n/locales/ko/index.js
@@ -1,9 +1,11 @@
 import todos from "./todos.json"
+import movies from "./movies.json"
 
 // 주의 : function형으로 사용하면 안됨!
 const ko = {
   // 이후 json 파일을 추가해서 사용한다.
   todos,
+  movies,
 }
 
 export default ko
