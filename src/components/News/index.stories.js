8491aae464ad9d323a83f91e4e7d0963a2a8d6b3
Junyeob <jy@Junyeobui-MacBookAir.local>
2023-04-08 11:56:24 +0900

    refactor: News컴포넌트storybook도입

---
+++ src/components/News/index.stories.js
@@ -0,0 +1,39 @@
+import React from "react"
+import { action } from "@storybook/addon-actions"
+import News from "./"
+
+export default {
+  title : "Components/News",
+  component : News,
+}
+  const sampleNews = [
+      {
+        points: 90,
+        id: 1,
+        url: "https://example.com",
+        title: "Example News",
+        user: "John Doe",
+      },
+      {
+        points: 70,
+        id: 2,
+        url: "https://example2.com",
+        title: "Example News 2",
+        user: "Jane Smith",
+      },
+      {
+        points: 50,
+        id: 3,
+        url: "https://example3.com",
+        title: "Example News 3",
+        user: "Bob Johnson",
+      },
+    ]
+
+    const Template = (args) => <News {...args} />
+    
+    export const Default = Template.bind({})
+    Default.args = {
+      news: sampleNews,
+      setNews: action("setNews"),
+    }
