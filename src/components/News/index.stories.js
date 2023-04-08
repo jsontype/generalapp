c44a8849a13c568312d31f93351dfd8e28813b50
jsontype <jsontyper@gmail.com>
2023-04-08 14:44:36 +0900

    fix: comment

--- src/components/News/index.stories.js
+++ src/components/News/index.stories.js
@@ -1,40 +1,40 @@
 import React from "react"
 import { action } from "@storybook/addon-actions"
-import News from "./"
+import News from "./News"
 
 export default {
-  title : "Components/News",
-  component : News,
+  title: "Components/News",
+  component: News,
 }
-  const sampleNews = [
-      {
-        points: 90,
-        id: 1,
-        url: "https://example.com",
-        title: "Example News",
-        user: "John Doe",
-      },
-      {
-        points: 70,
-        id: 2,
-        url: "https://example2.com",
-        title: "Example News 2",
-        user: "Jane Smith",
-      },
-      {
-        points: 50,
-        id: 3,
-        url: "https://example3.com",
-        title: "Example News 3",
-        user: "Bob Johnson",
-      },
-    ]
 
-    const Template = (args) => <News {...args} />
-    
-    export const Default = Template.bind({})
-    Default.args = {
-      news: sampleNews,
-      setNews: action("setNews"),
-    }
- 
+const sampleNews = [
+  {
+    points: 90,
+    id: 1,
+    url: "https://example.com",
+    title: "Example News",
+    user: "John Doe",
+  },
+  {
+    points: 70,
+    id: 2,
+    url: "https://example2.com",
+    title: "Example News 2",
+    user: "Jane Smith",
+  },
+  {
+    points: 50,
+    id: 3,
+    url: "https://example3.com",
+    title: "Example News 3",
+    user: "Bob Johnson",
+  },
+]
+
+const Template = (args) => <News {...args} />
+
+export const Default = Template.bind({})
+Default.args = {
+  news: sampleNews,
+  setNews: action("setNews"),
+}
